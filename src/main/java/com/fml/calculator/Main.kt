540c4f1834e1fd92a84315ad1c8ec0e202b90a84
Matt Busche <mrbusche@gmail.com>
2018-06-10 20:14:46 -0500

    Update to use full $ amounts

--- src/main/java/com/fml/calculator/Main.kt
+++ src/main/java/com/fml/calculator/Main.kt
@@ -4,23 +4,23 @@ object Main {
     private val startTime = System.currentTimeMillis()
 
     private val MOVIES = listOf(
-            Movie("Empty", 0, -200),
+            Movie("Empty", 0, -2000000),
             // Movies of the week
-            Movie("oceans 8", 626, 3923333),
-            Movie("hereditary", 219, 1014444),
-            Movie("solo", 218, 1380180),
-            Movie("deadpool 2", 201, 1267850),
-            Movie("hotel artemis", 138, 646666),
-            Movie("infinity", 93, 642500),
-            Movie("adrift", 89, 634350),
-            Movie("book club", 67, 478000),
-            Movie("upgrade", 32, 241875),
-            Movie("life of the party", 31, 202187),
-            Movie("breaking in", 24, 176000),
-            Movie("quiet place", 16, 110800),
-            Movie("overboard", 16, 102250),
-            Movie("action point", 15, 99050),
-            Movie("rbg", 11, 74300)
+            Movie("oceans 8", 626, 39233333),
+            Movie("hereditary", 219, 10144444),
+            Movie("solo", 218, 13801800),
+            Movie("deadpool 2", 201, 12678500),
+            Movie("hotel artemis", 138, 6466666),
+            Movie("infinity", 93, 6425000),
+            Movie("adrift", 89, 6343500),
+            Movie("book club", 67, 4780000),
+            Movie("upgrade", 32, 4418750),
+            Movie("life of the party", 31, 2021875),
+            Movie("breaking in", 24, 1760000),
+            Movie("quiet place", 16, 1108000),
+            Movie("overboard", 16, 1022500),
+            Movie("action point", 15, 990500),
+            Movie("rbg", 11, 743000)
     )
 
     @JvmStatic
